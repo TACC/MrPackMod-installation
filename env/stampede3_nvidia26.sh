877f8f0b79e9256dd9a78c90fc6e59f46545eb63
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-09-22 23:05:33 -0500

    nv 26

---
+++ env/stampede3_nvidia26.sh
@@ -0,0 +1,38 @@
+##
+## load OneAPI intel compiler
+##
+
+echo "module reset"
+module purge
+module reset
+# module list
+module load TACC
+
+echo "module unload intel and others"
+module unload intel oneapi gcc nvidia impi mvapich2 openmpi \
+    cuda python3 python2 2>/dev/null
+
+export VICTOR_WORK=/work2/00434/eijkhout/stampede3
+export MODULEROOT=${VICTOR_WORK}/modulefiles
+
+export VICTOR_MODULEPATH_ROOT=${MODULEROOT}
+module use ${VICTOR_MODULEPATH_ROOT}/Core
+
+nvversion=26.1
+echo "loading nvidia ${nvversion}"
+module load nvidia/${nvversion}
+
+# prepend for a modern libstdc++
+export LD_LIBRARY_PATH=/opt/apps/gcc/15.1.0/lib64/:${LD_LIBRARY_PATH}
+# append for libisl
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/apps/gcc/15.1.0/lib
+
+module load openmpi
+
+which modulelist >/dev/null 2>&1
+if [ $? -eq 0 ] ; then
+    modulelist
+else
+    module -t list 2>&1 | sort | awk '{v=v" "$0} END {print v}'
+fi
+
